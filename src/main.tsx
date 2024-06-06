59b722d790814d12691635545130fe05b612cff2
NathanLins12 <nathan.lins012@gmail.com>
2024-06-06 20:56:40 -0300

    add tasks in home

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import { ThemeProvider } from "styled-components";
 import { appTheme } from "./styles/theme";
 import { GlobalStyles } from "./styles/global";
 import { AuthProvider } from "./contexts/authContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
-      <ThemeProvider theme={appTheme}>
-        <AppRoutes />
-        <GlobalStyles />
-      </ThemeProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <ThemeProvider theme={appTheme}>
+          <AppRoutes />
+          <GlobalStyles />  
+        </ThemeProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
 );
